59f7e3e879ab4f94aa26187e0f44d5019d6cce27
Aram Koukia <aram.golbaghi@hotmail.com>
2017-01-05 15:21:17 -0800

    Initial solution commit

---
+++ DevOpsBot.Web/DevOpsBot.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevOpsBot.Web.MvcApplication" Language="C#" %>
---
+++ DevOpsBot.Web/DevOpsBot.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOpsBot.Web.Startup))]
+namespace DevOpsBot.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
